99bffff8100c0dd4b131b59827f7b161b2de36f5
Adam Weidner <adam@geome.company>
2019-07-11 15:44:06 -0400

    Adds getter and setter generation

--- src/main/java/com/schemaorg4j/codegen/factory/MethodContributor.java
+++ src/main/java/com/schemaorg4j/codegen/factory/MethodContributor.java
@@ -2,6 +2,9 @@ package com.schemaorg4j.codegen.factory;
 
 import com.schemaorg4j.codegen.domain.SchemaClass;
 import com.schemaorg4j.codegen.domain.SchemaGraph;
+import com.squareup.javapoet.FieldSpec;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.ParameterSpec;
 
 public class MethodContributor implements BlueprintContributor {
 
@@ -13,6 +16,29 @@ public class MethodContributor implements BlueprintContributor {
 
     @Override
     public void contribute(SchemaClass schemaClass, JavaPoetFileBlueprint blueprint) {
+        blueprint.getFields().forEach(fieldSpec -> {
+            blueprint.addMethod(contributeSetter(fieldSpec));
+            blueprint.addMethod(contributeGetter(fieldSpec));
+        });
+    }
 
+    private MethodSpec contributeGetter(FieldSpec fieldSpec) {
+        return MethodSpec
+            .methodBuilder("get" + capitalize(fieldSpec.name))
+            .addStatement("return $N", fieldSpec.name)
+            .returns(fieldSpec.type)
+            .build();
+    }
+
+    private String capitalize(String name) {
+        return name.substring(0, 1).toUpperCase() + name.substring(1);
+    }
+
+    private MethodSpec contributeSetter(FieldSpec fieldSpec) {
+        return MethodSpec
+            .methodBuilder("set" + capitalize(fieldSpec.name))
+            .addParameter(ParameterSpec.builder(fieldSpec.type, fieldSpec.name).build())
+            .addStatement("this.$N = $N", fieldSpec.name, fieldSpec.name)
+            .build();
     }
 }
