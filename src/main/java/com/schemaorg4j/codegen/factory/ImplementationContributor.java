1905b2ea60c74c50b3b5d5dfa1252fb610528508
Adam Weidner <adam@geome.company>
2019-07-12 13:25:13 -0400

    Adds functionality to generate implementation classes

--- src/main/java/com/schemaorg4j/codegen/factory/ImplementationContributor.java
+++ src/main/java/com/schemaorg4j/codegen/factory/ImplementationContributor.java
@@ -1,11 +1,22 @@
 package com.schemaorg4j.codegen.factory;
 
+import static com.schemaorg4j.codegen.constants.Schema4JConstants.DOMAIN_PACKAGE;
+import static com.schemaorg4j.codegen.factory.types.MethodUtil.getGetter;
+import static com.schemaorg4j.codegen.factory.types.MethodUtil.getSetter;
+
 import com.schemaorg4j.codegen.domain.SchemaClass;
 import com.schemaorg4j.codegen.domain.SchemaGraph;
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
+import com.squareup.javapoet.TypeSpec.Builder;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import javax.lang.model.element.Modifier;
 
 public class ImplementationContributor implements BlueprintContributor {
 
-    private SchemaGraph graph;
+    private final SchemaGraph graph;
 
     public ImplementationContributor(SchemaGraph graph) {
         this.graph = graph;
@@ -13,6 +24,28 @@ public class ImplementationContributor implements BlueprintContributor {
 
     @Override
     public void contribute(SchemaClass schemaClass, JavaPoetFileBlueprint blueprint) {
+        Builder builder = TypeSpec
+            .classBuilder(schemaClass.getLabel() + "Impl")
+            .addModifiers(Modifier.PUBLIC)
+            .addSuperinterface(ClassName.get(DOMAIN_PACKAGE, schemaClass.getLabel()));
+
+        builder.addFields(blueprint.getFields());
+        builder.addFields(blueprint.getInheritedFields());
+        builder.addMethods(blueprint.getMethods());
+        builder.addMethods(generateInheritedAccessorsAndMutators(blueprint));
+
+        blueprint.addType(builder.build());
+    }
+
+    private Iterable<MethodSpec> generateInheritedAccessorsAndMutators(JavaPoetFileBlueprint blueprint) {
+        return blueprint.getInheritedFields()
+            .stream()
+            .map(field -> Stream.of(
+                getGetter(field).toBuilder().addAnnotation(Override.class).build(),
+                getSetter(field).toBuilder().addAnnotation(Override.class).build())
+            )
+            .flatMap(stream -> stream)
+            .collect(Collectors.toList());
 
     }
 }
